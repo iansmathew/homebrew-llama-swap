70b4fcf9352c885b4e3118eaecd1457ca45e3bc1
Ian Sebastian Mathew <dev.iansmathew@gmail.com>
2025-07-30 01:15:32 +0530

    fix: changed sha for test

--- Formula/llama-swap.rb
+++ Formula/llama-swap.rb
@@ -7,22 +7,22 @@ class LlamaSwap < Formula
   on_macos do
     on_arm do
       url "https://github.com/mostlygeek/llama-swap/releases/download/v#{version}/llama-swap_#{version}_darwin_arm64.tar.gz"
-      sha256 "5010a4e21c1c94ccf46e6915f373667963d42c1a62ff39f7453cf6496c6c6852"
+      sha256 "test-sha"
     end
     on_intel do
       url "https://github.com/mostlygeek/llama-swap/releases/download/v#{version}/llama-swap_#{version}_darwin_amd64.tar.gz"
-      sha256 "734ee66be826a81b1cb2c0b4550d865a842918bc3d459f3d20d083d390aa0809"
+      sha256 "test-sha"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/mostlygeek/llama-swap/releases/download/v#{version}/llama-swap_#{version}_linux_arm64.tar.gz"
-      sha256 "10580f69b49f011d310e3d8555a58933f5d18b5467f17f3d503265a410529fda"
+      sha256 "test-sha"
     end
     on_intel do
       url "https://github.com/mostlygeek/llama-swap/releases/download/v#{version}/llama-swap_#{version}_linux_amd64.tar.gz"
-      sha256 "e2901e5aa2f7a2fd1f4ff7659d91d2b98a1cd470eff6cff04ae89f6b235042f7"
+      sha256 "test-sha"
     end
   end
 
